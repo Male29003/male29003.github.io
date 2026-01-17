ce3fff1ffe2b7b6451fdf904cadab470755a5978
Male29003 <nam2309200312345@gmail.com>
2026-01-17 20:13:29 +0700

    ..

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,9 +1,7 @@
-import SectionTitle from "../components/SectionTitle"
 
 export default function Projects() {
   return (
     <section id="projects" className="py-32">
-        <SectionTitle>Projects</SectionTitle>
         <p className="max-w-2xl opacity-80">
           I'm a frontend developer focused on building clean, accessible,
           and delightful user experiences.
