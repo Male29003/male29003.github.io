e8b5bf4e81898ce91eaecec2428a2833e7cb7f98
Male29003 <nam2309200312345@gmail.com>
2026-01-24 08:32:48 +0700

    Track icons only

---
+++ src/assets/icons/index.jsx
@@ -0,0 +1,28 @@
+import reactIcon from "../icons/frameworks/react.svg"
+import tailwindIcon from "../icons/frameworks/tailwindcss.png"
+import nodejsIcon from "../icons/frameworks/nodedotjs.png"
+
+import pythonIcon from "../icons/pr_languages/python.png"
+import javascriptIcon from "../icons/pr_languages/javascript.png"
+import typescriptIcon from "../icons/pr_languages/typescript.png"
+import phpIcon from "../icons/pr_languages/php.png"
+import cssIcon from "../icons/pr_languages/css.svg"
+
+import mysqlIcon from "../icons/db/mysql.png"
+import postgreSqlIcon from "../icons/db/postgresql.png"
+
+import figmaIcon from "../icons/tools/figma.png"
+
+export {
+    reactIcon,
+    tailwindIcon,
+    nodejsIcon,
+    pythonIcon,
+    javascriptIcon,
+    typescriptIcon,
+    phpIcon,
+    cssIcon,
+    mysqlIcon,
+    postgreSqlIcon,
+    figmaIcon,
+}
